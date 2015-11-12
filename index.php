25b060a8a2f3388516d33798e52425b12ccf0aa3
Andrey Filimonov <andrefilimono@gmail.com>
2015-11-12 22:33:11 +0300

    Fixed html comment

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><html class="no-js"><!--<![endif]-->
+<!--[if gt IE 8]><html class="no-js"><![endif]-->
 <html lang="ru-RU">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
